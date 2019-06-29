4f3b1fdf25eaedd38bc69e720c289a0eed8717cf
Vlad <vlad300820001@gmail.com>
2019-06-29 07:45:50 +0300

    Final commit

--- src/com/vlad/Task11/Main.java
+++ src/com/vlad/Task11/Main.java
@@ -1,5 +1,6 @@
 package com.vlad.Task11;
 
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         RecipeStorage recipeStorage = new RecipeStorage();
@@ -10,7 +11,6 @@ public class Main {
 
         recipeStorage.getRecipeForProlongation("Recipe for Josh","Prolonged Recipe for Josh","30/12/2014");
 
-        recipeStorage.getRecipy("Recipe for Josh");
 
         recipeStorage.getRecipy("Prolonged Recipe for Josh");
 
